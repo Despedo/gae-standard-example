dd20f58e05eb351a6133ca9161d954132cb9e927
oleksandr_vechirko <oleksandr_vechirko@epam.com>
2019-09-15 23:51:14 +0300

    Added random

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,24 +2,27 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.logging.Logger;
 
 @SpringBootApplication
 @RestController
 public class DemoApplication {
 
- private static final Logger logger = Logger.getLogger(DemoApplication.class.getName());
+    private static final Logger logger = Logger.getLogger(DemoApplication.class.getName());
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
-  @GetMapping("/")
-  public String hello() {
-    logger.info("Logging INFO with java.util.logging");
-    logger.severe("Logging ERROR with java.util.logging");
-    return "hello world!";
-  }
+    @GetMapping("/")
+    public String hello() {
+        double random = Math.random();
+        logger.info("Logging INFO with java.util.logging");
+        logger.severe("Logging ERROR with java.util.logging");
+        return "hello world!";
+    }
 
 }
