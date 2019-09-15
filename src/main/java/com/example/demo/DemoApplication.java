984f26fe69b3e51259d242b5be08a63d7ba46bfe
oleksandr_vechirko <oleksandr_vechirko@epam.com>
2019-09-16 01:34:55 +0300

    Enabled logging

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,16 +3,16 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//import java.util.logging.Logger;
+import java.util.logging.Logger;
 
 @SpringBootApplication
 
 public class DemoApplication {
 
-//    private static final Logger logger = Logger.getLogger(DemoApplication.class.getName());
+    private static final Logger logger = Logger.getLogger(DemoApplication.class.getName());
 
     public static void main(String[] args) {
-//        logger.info("Application starting");
+        logger.info("Application starting");
         SpringApplication.run(DemoApplication.class, args);
     }
 
--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,26 +1,21 @@
 package com.example.demo;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Random;
-//import java.util.logging.Logger;
+import java.util.logging.Logger;
 
 @RestController
 public class DemoController {
 
-//    private static final Logger logger = Logger.getLogger(DemoController.class.getName());
-private static final Logger logger = LoggerFactory.getLogger(DemoController.class);
+    private static final Logger logger = Logger.getLogger(DemoController.class.getName());
 
     @GetMapping("/")
     public String hello() {
         int random = new Random().nextInt(20);
-        logger.info("Logging INFO with Logback");
-        logger.error("Logging ERROR with Logback");
-//        logger.info("Logging INFO with java.util.logging");
-//        logger.severe("Logging ERROR with java.util.logging");
+        logger.info("Logging INFO with java.util.logging");
+        logger.severe("Logging ERROR with java.util.logging");
         return "Hello world! Random is: " + random;
     }
 }
