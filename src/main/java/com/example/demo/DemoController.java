0dc8a3fd3cae24258d584f90965aec2121ba551b
oleksandr_vechirko <oleksandr_vechirko@epam.com>
2019-09-16 00:48:07 +0300

    Disabled logging

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -9,13 +9,13 @@ import java.util.logging.Logger;
 @RestController
 public class DemoController {
 
-    private static final Logger logger = Logger.getLogger(DemoController.class.getName());
+//    private static final Logger logger = Logger.getLogger(DemoController.class.getName());
 
     @GetMapping("/")
     public String hello() {
         int random = new Random().nextInt(20);
-        logger.info("Logging INFO with java.util.logging");
-        logger.severe("Logging ERROR with java.util.logging");
+//        logger.info("Logging INFO with java.util.logging");
+//        logger.severe("Logging ERROR with java.util.logging");
         return "Hello world! Random is: " + random;
     }
 }
