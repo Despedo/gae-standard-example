c27185e1783fc871e5051d5205d0f5ea9bf559c6
oleksandr_vechirko <oleksandr_vechirko@epam.com>
2019-09-15 23:18:43 +0300

    Simple project

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import java.util.logging.Logger;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+ private static final Logger logger = Logger.getLogger(DemoApplication.class.getName());
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+  @GetMapping("/")
+  public String hello() {
+    logger.info("Logging INFO with java.util.logging");
+    logger.severe("Logging ERROR with java.util.logging");
+    return "hello world!";
+  }
+
+}
